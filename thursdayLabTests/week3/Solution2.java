26bd159424ced83aa14d09d8faccf2e0dac88289
fxml21 <103354270+fxml21@users.noreply.github.com>
2022-10-06 19:50:45 +0530

    Create Solution2.java

---
+++ thursdayLabTests/week3/Solution2.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Solution {
+      private static void reverse(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            swap(arr, left++, right--);
+        }
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        // Take inputs here
+        Scanner sc = new Scanner(System.in); 
+        int N = sc.nextInt(); 
+        // Reverse the array using reverse method
+        int[] a = new int[N]; 
+        for (int i = 0; i < N; i++){
+            a[i] = sc.nextInt(); 
+        }
+        // Output:      
+        reverse(a); 
+        System.out.println(Arrays.toString(a));
+    }
+}
