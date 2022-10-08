48444bd4496c7292f1b8f153132f2056790ca167
fxml21 <103354270+fxml21@users.noreply.github.com>
2022-10-08 20:08:08 +0530

    Create Programmer.java

---
+++ thursdayLabTests/week5/task1/Programmer.java
@@ -0,0 +1,20 @@
+public class Programmer extends Employee {
+    private final double bonus;
+    private final String specialization;
+
+    public Programmer(String name, double salary, double bonus, String specialization) {
+        super(name, salary);
+        this.bonus = bonus;
+        this.specialization = specialization;
+    }
+
+    @Override
+    public double getSalary() {
+        return super.getSalary() + bonus;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "; Programmer for " + specialization;
+    }
+}
