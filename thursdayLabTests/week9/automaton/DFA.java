7cf2a22c1f8ffd42d2232cbc48e4b4c61878cf70
fxml21 <103354270+fxml21@users.noreply.github.com>
2022-11-17 17:20:00 +0530

    Create DFA.java

---
+++ thursdayLabTests/week9/automaton/DFA.java
@@ -0,0 +1,63 @@
+// Remove public in hackerrank submission
+public class DFA implements Automaton {
+    private final Map<State, Map<Character, State>> transitionFunction = new HashMap<>();
+    private final State startState;
+    private final List<State> acceptStates;
+    private final String alphabet;
+
+    public DFA(State[] states, char[] inputAlphabet, List<Edge> transitions, State startState) {
+        this.startState = startState;
+        alphabet = new String(inputAlphabet);
+        this.acceptStates = new ArrayList<>();
+        for (State state : states) {
+            if (state.isAcceptState()) {
+                acceptStates.add(state);
+            }
+            transitionFunction.put(state, new HashMap<>());
+        }
+        for (Edge transition : transitions) {
+            if (alphabet.indexOf(transition.getOn()) == -1) {
+                throw new IllegalArgumentException("Illegal transition for DFA; cannot construct it!");
+            }
+            transitionFunction.get(transition.getFrom()).put(transition.getOn(), transition.getTo());
+        }
+    }
+
+    @Override
+    public void simulate(String input) {
+        State current = startState;
+        System.out.print(current);
+        for (int i = 0; i < input.length(); i++) {
+            char in = input.charAt(i);
+            if (alphabet.indexOf(in) == -1) {
+                throw new IllegalArgumentException("\nIllegal input character " + in  + "; DFA dies!" );
+            }
+            State next = transitionFunction.get(current).get(input.charAt(i));
+            System.out.print(" on " + input.charAt(i) + " --> " + next);
+            current = next;
+        }
+
+        if (current.isAcceptState()) {
+            System.out.println("\nDFA accepted the string " + input);
+        } else {
+            System.out.println("\nDFA rejected the string " + input);
+        }
+    }
+
+    @Override
+    public List<State> getAcceptStates() {
+        return acceptStates;
+    }
+
+    public Map<State, Map<Character, State>> getTransitionFunction() {
+        return transitionFunction;
+    }
+
+    public State getStartState() {
+        return startState;
+    }
+
+    public String getAlphabet() {
+        return alphabet;
+    }
+}
