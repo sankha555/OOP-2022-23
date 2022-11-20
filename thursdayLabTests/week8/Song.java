d0a96f6336d979ae42b5dc13b395b5b2eea85ea7
fxml21 <103354270+fxml21@users.noreply.github.com>
2022-11-20 15:55:55 +0530

    Create Song.java

---
+++ thursdayLabTests/week8/Song.java
@@ -0,0 +1,46 @@
+class Song implements Comparable<Song> {
+    private final String artist;
+    private final int durationInSeconds;
+    private final long hearts;
+    private boolean hasSpatialAudio;
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+    private final String name;
+
+    public Song(String name, String artist, int durationInSeconds, long hearts, boolean hasSpatialAudio) {
+        this.name = name;
+        this.artist = artist;
+        this.durationInSeconds = durationInSeconds;
+        this.hearts = hearts;
+        this.hasSpatialAudio = hasSpatialAudio;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getDurationInSeconds() {
+        return durationInSeconds;
+    }
+
+    public boolean getHasSpatialAudio() {
+        return hasSpatialAudio;
+    }
+
+    public long getHearts() {
+        return hearts;
+    }
+
+    @Override
+    public int compareTo(Song o) {
+        return Integer.compare(this.durationInSeconds, o.durationInSeconds);
+    }
+
+    public void setHasSpatialAudio(boolean hasSpatialAudio) {
+        this.hasSpatialAudio = hasSpatialAudio;
+    }
+}
