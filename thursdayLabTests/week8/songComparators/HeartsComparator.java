7f283dd7f5daa9b4e996bb4758090a6874506c21
fxml21 <103354270+fxml21@users.noreply.github.com>
2022-11-20 16:02:33 +0530

    Create HeartsComparator.java

---
+++ thursdayLabTests/week8/songComparators/HeartsComparator.java
@@ -0,0 +1,14 @@
+import java.util.Comparator;
+// Remove public in hackerank submission, move imports to top in hackerrank submission
+public class HeartsComparator implements Comparator<Song> {
+
+    @Override
+    public int compare(Song o1, Song o2) {
+        if (o1.getHearts() == o2.getHearts()) return o1.compareTo(o2);
+        if (o1.getHearts() > o2.getHearts()) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
+}
